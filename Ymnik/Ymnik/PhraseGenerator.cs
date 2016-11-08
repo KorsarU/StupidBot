473f80138b4329381fdccdbae749f7826e24eb6b
KorsarU <KorsarUTrue@gmail.com>
2016-11-09 02:42:20 +0300

    Just fer init

---
+++ Ymnik/Ymnik/PhraseGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Ymnik
+{
+    internal class PhraseGenerator
+    {
+        private ArrayList dictionary;
+
+        public PhraseGenerator()
+        {
+            dictionary = new ArrayList();
+            loadDict();
+        }
+
+        public string Talk(string input)
+        {
+            var phrase = PickClosesPhrase(input);
+            return phrase;
+        }
+
+        private string PickClosesPhrase(string input)
+        {
+            /**/
+            Random phraseLenght = new Random();
+            String[] phrases = GenerateRandomPhrases(phraseLenght.Next(0, 20), 1000);/*пока константы, гугл не всегда может съесть дохрена фраз*/
+        }
+
+        private string[] GenerateRandomPhrases(int wordsCount, int countOfPhrases)
+        {
+            Random rndWord = new Random();
+            HashSet<string> phrases = new HashSet<string>();
+            for (int i = 0; i < 1000; i++)
+            {
+                string generatingPhrase = "";
+                for (int j = 0; j < wordsCount; j++)
+                {
+                    generatingPhrase += dictionary[rndWord.Next(0, dictionary.Count)];
+                }
+            }
+        }
+    }
+}
---
+++ Ymnik/Ymnik/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ymnik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            string output = "";
+            PhraseGenerator stupidBot = new PhraseGenerator();
+            output = stupidBot.Talk(input);
+        }
+    }
+}
